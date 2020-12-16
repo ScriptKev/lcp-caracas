4787c905ca63b334ba7de58dd0df47f6b4fbd9c9
ScriptKev <script.kev@gmail.com>
2020-12-16 07:02:25 -0500

    Testeando variables de entorno privadas sexto intento

--- services/googleApi/index.js
+++ services/googleApi/index.js
@@ -1,43 +1,41 @@
-// import { TotalReservationsUpdateIncrement } from 'services/firebase'
+import { TotalReservationsUpdateIncrement } from 'services/firebase'
 
-const BASE_URL = process.env.NEXT_PUBLIC_NO_CODE_API_BAES_URL
-const TabSheetId = process.env.NEXT_PUBLIC_GOOGLE_DOCS_EXCEL_TAB_SHEET_ID
+const BASE_URL = process.env.NEXT_PUBLIC_GOOGLE_SHEETS_BASE_URL
+const TabSheetId = process.env.NEXT_PUBLIC_TAB_SHEET_ID
 
 export const AddReservation = async (reservation) => {
-  console.log('Add Reservaton', BASE_URL, TabSheetId)
-  console.log('env.Local', process.env.DB_PASS, process.env.DB_USER, process.env.DB_HOST)
-  // let { name, lastname, email, phone, worshipShedule, reservations: { value } } = reservation
-  // if (email === '' || email === ' ') email = 'Sin correo'
-  // const date = new Date();
-  // const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  // const dateLocal = date.toLocaleDateString("es-ES", options)
+  let { name, lastname, email, phone, worshipShedule, reservations: { value } } = reservation
+  if (email === '' || email === ' ') email = 'Sin correo'
+  const date = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const dateLocal = date.toLocaleDateString("es-ES", options)
 
-  // const newReservation = [
-  //   dateLocal,
-  //   `${name} ${lastname}`,
-  //   phone,
-  //   email,
-  //   Number(value),
-  //   worshipShedule,
-  // ]
+  const newReservation = [
+    dateLocal,
+    `${name} ${lastname}`,
+    phone,
+    email,
+    Number(value),
+    worshipShedule,
+  ]
 
-  // try {
-  //   const res = await fetch(`${BASE_URL}?tabId=${TabSheetId}`, {
-  //     mode: 'cors',
-  //     method: "post",
-  //     body: JSON.stringify([newReservation]),
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Origin": "*"
-  //     }
-  //   })
+  try {
+    const res = await fetch(`${BASE_URL}?tabId=${TabSheetId}`, {
+      mode: 'cors',
+      method: "post",
+      body: JSON.stringify([newReservation]),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
 
-  //   const data = await res.json()
-  //   console.log('Success:', JSON.stringify(data))
+    const data = await res.json()
+    console.log('Success:', JSON.stringify(data))
 
-  // } catch (err) {
-  //   console.error(err)
-  // }
+  } catch (err) {
+    console.error(err)
+  }
 
-  // TotalReservationsUpdateIncrement(worshipShedule, Number(value))
+  TotalReservationsUpdateIncrement(worshipShedule, Number(value))
 }
