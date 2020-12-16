7eac0990d7015b12cfb72ae18bff01cdc1e42f88
ScriptKev <script.kev@gmail.com>
2020-12-16 12:49:05 -0500

    Path absolute agregado al storybook

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,6 +1,7 @@
+import Button from 'components/Button';
 import React from 'react';
 
-import { Button } from './Button';
+// import { Button } from './Button';
 
 export default {
   title: 'Example/Button',
